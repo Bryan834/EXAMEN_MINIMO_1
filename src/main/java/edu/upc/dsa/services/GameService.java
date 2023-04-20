0fa9128c2f5918927d081c0bfd04e2d05a9b0929
bryan834 <bryan.garcia@estudiantat.upc.edu>
2023-04-20 11:18:46 +0200

    EXAMEN 2022 2023 QP

---
+++ src/main/java/edu/upc/dsa/services/GameService.java
@@ -0,0 +1,25 @@
+package edu.upc.dsa.services;
+
+import edu.upc.dsa.GameManager;
+import edu.upc.dsa.GameManagerImpl;
+import edu.upc.dsa.models.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+
+@Api(value = "/game")
+@Path("/")
+public class GameService {
+
+
+
+
+}
